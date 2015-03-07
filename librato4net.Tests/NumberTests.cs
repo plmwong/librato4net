6128fabc63d82d1cd2c272eefde8561a369f2f5e
Phillip Wong <wongphil@gmail.com>
2015-03-06 20:38:08 -0800

    Added tests for Number.

---
+++ librato4net.Tests/NumberTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace librato4net.Tests
+{
+	[TestFixture]
+	public class NumberTests
+	{
+		[Test]
+		public void integer_can_be_placed_into_number() 
+		{
+			Number number = 24;
+
+			Assert.That(number.IntegerValue, Is.EqualTo(24));
+			Assert.That(number.FloatValue, Is.Null);
+		}
+
+		[Test]
+		public void float_can_be_placed_into_number() 
+		{
+			Number number = 45.0f;
+
+			Assert.That(number.FloatValue, Is.EqualTo(45.0f));
+			Assert.That(number.IntegerValue, Is.Null);
+		}
+	}
+}
+
