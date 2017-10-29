46eb7cfbb64708ff7f3e88655f15d5e8c310449b
Phillip Wong <wongphil@gmail.com>
2017-10-29 00:10:31 -0700

    Added AppSettingsLibratoSettings tests.

---
+++ librato4net.Tests/AppSettingsLibratoSettings.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace librato4net.Tests
+{
+    [TestFixture]
+    public class AppSettingsLibratoSettingsTests
+    {
+        [Test]
+        public void can_read_apikey_from_application_configuration()
+        {
+            Assert.That(LibratoSettings.Settings.ApiKey, Is.EqualTo("YOUR_API_KEY"));
+        }
+
+        [Test]
+        public void can_read_username_from_application_configuration()
+        {
+            Assert.That(LibratoSettings.Settings.Username, Is.EqualTo("YOUR_USERNAME"));
+        }
+    }
+}
+
--- librato4net.Tests/LibratoSettingsTests.cs
+++ librato4net.Tests/LibratoSettingsTests.cs
@@ -5,16 +5,24 @@ namespace librato4net.Tests
     [TestFixture]
     public class LibratoSettingsTests
     {
-        [Test]
-        public void can_read_apikey_from_application_configuration()
+        private ILibratoSettings _settings;
+        
+        [SetUp]
+        public void SetUp()
         {
-            Assert.That(LibratoSettings.Settings.ApiKey, Is.EqualTo("YOUR_API_KEY"));
+            _settings = new AppSettingsLibratoSettings();
+        }
+        
+        [Test]
+        public void can_read_apikey_from_appsettings_configuration()
+        {
+            Assert.That(_settings.ApiKey, Is.EqualTo("YOUR_APPSETTINGS_API_KEY"));
         }
 
         [Test]
-        public void can_read_username_from_application_configuration()
+        public void can_read_username_from_appsettings_configuration()
         {
-            Assert.That(LibratoSettings.Settings.Username, Is.EqualTo("YOUR_USERNAME"));
+            Assert.That(_settings.Username, Is.EqualTo("YOUR_APPSETTINGS_USERNAME"));
         }
     }
 }
